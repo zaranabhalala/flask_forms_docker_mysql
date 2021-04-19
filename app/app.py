5eed8febe1f8c3fa9dbc4df697adac5eaa0c4f18
zaranabhalala <zaranabhalala1996@gmail.com>
2021-04-19 17:26:03 -0400

    Feature: Added code for APIs

--- app/app.py
+++ app/app.py
@@ -15,6 +15,7 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'mlbPlayers'
 mysql.init_app(app)
 
+
 @app.route('/', methods=['GET'])
 def index():
     user = {'username': 'Name Project'}
@@ -23,6 +24,7 @@ def index():
     result = cursor.fetchall()
     return render_template('index.html', title='Home', user=user, Names=result)
 
+
 @app.route('/view/<string:Name>', methods=['GET'])
 def record_view(Name):
     cursor = mysql.get_db().cursor()
@@ -30,12 +32,14 @@ def record_view(Name):
     result = cursor.fetchall()
     return render_template('view.html', title='View Form', Player=result[0])
 
+
 @app.route('/edit/<string:Name>', methods=['GET'])
 def form_edit_get(Name):
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM mlb_players WHERE Name=%s', Name)
     result = cursor.fetchall()
     return render_template('edit.html', title='Edit Form', Player=result[0])
+
 
 @app.route('/edit/<string:Name>', methods=['POST'])
 def form_update_post(Name):
@@ -49,9 +53,11 @@ def form_update_post(Name):
     mysql.get_db().commit()
     return redirect("/", code=302)
 
+
 @app.route('/names/new', methods=['GET'])
 def form_insert_get():
     return render_template('new.html', title='New Player Form')
+
 
 @app.route('/Names/new', methods=['POST'])
 def form_insert_post():
@@ -64,6 +70,54 @@ def form_insert_post():
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
+
+
+@app.route('/delete/<string:Name>', methods=['POST'])
+def form_delete_post(Name):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM mlb_players WHERE Name = %s """
+    cursor.execute(sql_delete_query, Name)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/Names', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM mlb_players')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/Names/<string:Name>', methods=['GET'])
+def api_retrieve(Name) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM mlb_players WHERE Name=%s', Name)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/Names/', methods=['POST'])
+def api_add() -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/Names/<string:Name>', methods=['PUT'])
+def api_edit(name) -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/Names/<string:Name>', methods=['DELETE'])
+def api_delete(name) -> str:
+    resp = Response(status=210, mimetype='application/json')
+    return resp
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
